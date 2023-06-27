5411a5918c26f789af5fbe412728ae417eb344e4
Gustavo Pantoja <gustavopantoja.ap@gmail.com>
2023-06-27 15:30:30 -0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Models\Taxas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('v2/auth', [\App\Http\Controllers\Api\UserAuthController::class,'auth']);
+
 
 Route::post('auth', [\App\Http\Controllers\Api\UsuarioAuthController::class, 'auth']);
 
