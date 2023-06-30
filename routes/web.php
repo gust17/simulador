150857fd8007e648503a8a57e718e4fcda7fcc61
Gustavo Pantoja <gustavopantoja.ap@gmail.com>
2023-06-30 11:57:05 -0300

    teste para consulta

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('padrao.padrao');
+    return view('simulador');
     $users = \App\Models\UserSistema::all();
     $user = \App\Models\UserSistema::find(458);
 
